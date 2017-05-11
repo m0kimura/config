e711765333c416b544912a29c6eb0622912efdd8
Michihiro Kimura <kimura@kmrweb.net>
2017-05-11 10:08:04 +0900

    docker setup correct

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 #
 #
-sudo su
-apt-get update
-apt-get install docker.io
-ln -sf /usr/bin/docker.io /usr/local/bin/docker
-service start docker
-docker run hello-world
+sudo apt-get update
+sudo apt-get install docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo gpasswd -a $USER docker
+sudo service start docker
+sudo docker run hello-world
 
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose version
+sudo curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > temp
+sudo cp temp /usr/local/bin/docker-compose
+sudo rm temp
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose version
+
