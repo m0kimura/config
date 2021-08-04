1a65c9a1a473f7dd62d2985fe561fa990139af83
Michihiro Kimura <kimura@kmrweb.net>
2021-08-04 18:40:49 +0900

    comment update

--- setup_base.sh
+++ setup_base.sh
@@ -3,20 +3,20 @@
 ##
 read -p "JAPANESE SETTING 実行しますか? (y/n)" x
 if [ "${x}" = "y" ] ; then
-  echo /##
-  echo /## aptitude ###
-  echo /##
+  echo "##"
+  echo "## aptitude ##"
+  echo "##"
   sudo apt install aptitude
 
-  echo /##
-  echo /## local time setting ###
-  echo /##
+  echo "##"
+  echo "## local time setting ##"
+  echo "##"
   sudo rm -rf /etc/localtime
   sudo ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
-  echo /##
-  echo /## japanese configration ###
-  echo /##
+  echo "##"
+  echo "## japanese configration ##"
+  echo "##"
   sudo aptitude install locales
   sudo aptitude install language-pack-ja
   sudo dpkg-reconfigure locales
@@ -29,9 +29,9 @@ fi
 
 read -p "NODEJS INSTALL 実行しますか? (y/n)" x
 if [ "${x}" = "y" ] ; then
-  echo /##
-  echo /## nodejs ###
-  echo /##
+  echo "##"
+  echo "## nodejs ##"
+  echo "##"
 
   sudo apt install nodejs npm
   sudo npm install -g n
@@ -44,9 +44,9 @@ fi
 
 read -p "AWSCLI INSTALL 実行しますか? (y/n)" x
 if [ "${x}" = "y" ] ; then
-  echo /##
-  echo /## awscli ###
-  echo /##
+  echo "##"
+  echo "## awscli ##"
+  echo "##"
   wget https://bootstrap.pypa.io/get-pip.py
   sudo python get-pip.py
   sudo pip install awscli
@@ -66,6 +66,6 @@ if [ "${x}" = "y" ] ; then
   sudo apt -y install docker-compose
 fi
 
-echo /##
-echo /##
-echo /## end ###
+echo "##"
+echo "##"
+echo "## end ##"
