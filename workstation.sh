590c1bbcfcee3f604f045907ff7fc3991e7eca65
Michihiro Kimura <kimura@kmrweb.net>
2021-08-04 12:36:19 +0900

    workstation.sh chrome追加

--- workstation.sh
+++ workstation.sh
@@ -26,3 +26,11 @@ if [ "${x}" = "y" ] ; then
   sudo apt install ./vscode.deb
   rm ./vscode.deb
 fi
+
+read -p "GOOGLE CHROME (y/n)" x
+if [ "${x}" = "y" ] ; then
+  sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+  sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo apt update
+  sudo apt install google-chrome-stable
+fi
